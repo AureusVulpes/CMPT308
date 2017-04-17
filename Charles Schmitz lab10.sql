c4d5ffd8a88e6af0dd85a7f281d3976463841d1f
Charles Schmitz <aureusvulpes@gmail.com>
2017-04-17 12:56:44 -0400

    Uploaded files for labs and project

---
+++ Charles Schmitz lab10.sql
@@ -0,0 +1,135 @@
+﻿----------------------------------------------------------------------------------------
+-- Courses and Prerequisites
+-- by Alan G. Labouseur
+-- Tested on Postgres 9.3.2
+----------------------------------------------------------------------------------------
+/*
+--
+-- The table of courses.
+--
+create table Courses (
+    num      integer not null,
+    name     text    not null,
+    credits  integer not null,
+  primary key (num)
+);
+
+
+insert into Courses(num, name, credits)
+values (499, 'CS/ITS Capping', 3 );
+
+insert into Courses(num, name, credits)
+values (308, 'Database Systems', 4 );
+
+insert into Courses(num, name, credits)
+values (221, 'Software Development Two', 4 );
+
+insert into Courses(num, name, credits)
+values (220, 'Software Development One', 4 );
+
+insert into Courses(num, name, credits)
+values (120, 'Introduction to Programming', 4);
+
+select * 
+from courses
+order by num ASC;
+
+
+--
+-- Courses and their prerequisites
+--
+create table Prerequisites (
+    courseNum integer not null references Courses(num),
+    preReqNum integer not null references Courses(num),
+  primary key (courseNum, preReqNum)
+);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (499, 308);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (499, 221);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (308, 120);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (221, 220);
+
+insert into Prerequisites(courseNum, preReqNum)
+values (220, 120);
+
+select *
+from Prerequisites
+order by courseNum DESC;
+
+
+
+
+--
+-- An example stored procedure ("function")
+--
+create or replace function get_courses_by_credits(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   num_credits int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select num, name, credits
+      from   courses
+       where  credits >= num_credits;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select get_courses_by_credits(0, 'results');
+Fetch all from results;
+*/
+
+
+
+--begin scripts for lab 10 by Charles Schmitz
+
+
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as
+--returns immediate prerequisites from passed in course number
+$$
+declare
+   course int :=$1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for   
+	select d.name from courses c join prerequisites p on c.num = p.courseNum join courses d on p.prereqnum = d.num 
+	where d.num = course;
+   return resultset;
+end;
+$$
+language plpgsql;
+
+select PreReqsFor(308, 'needs');
+Fetch all from needs;
+
+
+
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as
+--returns courses where the passed in number is an immediate prereq
+$$
+declare
+   course int :=$1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for   
+	select c.name from courses d join prerequisites p on d.num = p.prereqnum join courses c on p.courseNum = c.num 
+	where d.num = course;
+   return resultset;
+end;
+$$
+language plpgsql;
+
+select IsPreReqFor(308, 'allows');
+Fetch all from allows;
+
+
+
---
+++ SchmitzPokemonDB.sql
@@ -0,0 +1,124 @@
+﻿--create type table--
+DROP TABLE IF EXISTS Types;
+CREATE TABLE Types (
+	tid	text not null,
+	wknsNormal numeric(2,1),
+	wknsFire numeric(2,1),
+	wknsWater numeric(2,1),
+	wknsElectric numeric(2,1),
+	wknsGrass numeric(2,1),
+	wknsIce numeric(2,1),
+	wknsFighting numeric(2,1),
+	wknsPoison numeric(2,1),
+	wknsGround numeric(2,1),
+	wknsFlying numeric(2,1),
+	wknsPsychic numeric(2,1),
+	wknsBug numeric(2,1),
+	wknsRock numeric(2,1),
+	wknsGhost numeric(2,1),
+	wknsDragon numeric(2,1),
+	wknsDark numeric(2,1),
+	wknsSteel numeric(2,1),
+	wknsFairy numeric(2,1),
+	primary key(tid)
+);
+
+INSERT INTO Types( tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Normal', 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Fire', 1, .5, 2, 1, .5, .5, 1, 1, 2, 1, 1, .5, 2, 1, 1, 1, .5, .5);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Water', 1, .5, .5, 2, 2, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, .5, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Electric', 1, 1, 1, .5, 1, 1, 1, 1, 2, .5, 1, 1, 1, 1, 1, 1, .5, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Grass', 1, 2, .5, .5, .5, 2, 1, 2, .5, 2, 1, 2, 1, 1, 1, 1, .5, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Ice', 1, 2, 1, 1, 1, .5, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Fighting', 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, .5, .5, 1, 1, .5, 1, 2);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Poison', 1, 1, 1, 1, .5, 1, .5, .5, 2, 1, 2, .5, 1, 1, 1, 1, 1, .5);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Ground', 1, 1, 2, 0, 2, 2, 1, .5, 1, 1, 1, 1, .5, 1, 1, 1, 1, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Flying', 1, 1, 1, 2, 1, 2, .5, 1, 0, 1, 1, .5, 2, 1, 1, 1, 1, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Psychic', 1, 1, 1, 1, 1, 1, .5, 1, 1, 1, .5, 2, 1, 2, 1, 2, 1, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Bug', 1, 2, 1, 1, .5, 1, .5, 1, .5, 2, 1, 1, 2, 1, 1, 1, 1, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Rock', .5, .5, 2, 1, 2, 1, 2, .5, 2, .5, 1, 1, 1, 1, 1, 1, 2, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Ghost', 0, 1, 1, 1, 1, 1, 0, .5, 1, 1, 1, .5, 1, 2, 1, 2, 1, 1);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Dragon', 1, .5, .5, .5, .5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Dark', 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0, 2, 1, .5, 1, .5, 1, 2);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Steel', .5, 2, 1, 1, .5, .5, 2, 0, 2, .5, .5, .5, .5, 1, .5, 1, .5, .5);
+INSERT INTO Types (tid, wknsNormal, wknsFire, wknsWater, wknsElectric, wknsGrass,
+	wknsIce, wknsFighting, wknsPoison, wknsGround, wknsFlying, wknsPsychic,	
+	wknsBug, wknsRock, wknsGhost, wknsDragon, wknsDark, wknsSteel, wknsFairy) 
+	VALUES('Fairy', 1, 1, 1, 1, 1, 1, .5, 2, 1, 1, 1, .5, 1, 1, 0, .5, 2, 1);
+
+--types table properly builds
+--select * from types
+
+--create pokemon table
+DROP TABLE IF EXISTS Pokemon;
+CREATE TABLE Pokemon (
+  dexID     numeric(4,0) not null,
+  species   text,
+  Type1     text not null references Types(tid),
+  Type2     text references Types(tid),
+  baseHP    integer not null,
+  baseAtk   integer not null,
+  baseDef   integer not null,
+  baseSpAtk integer not null,
+  baseSpDef integer not null,
+  baseSpd   integer not null,
+  primary key(dexID)
+);
+
+--add a few basic test pokemon--
+INSERT INTO Pokemon(dexID, species, Type1, Type2, baseHP, baseAtk, baseDef, baseSpAtk, baseSpDef, baseSpd)
+	VALUES(0001, 'Charmander', 'Fire', NULL, 39, 52, 43, 60, 50, 65 );
+
+
+--verify results--
+SELECT * FROM Pokemon;
